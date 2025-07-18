386d53fdbb52aa95f7f032f0e77bfdee4aee9652
Stars Momodu <starsmomodu@gmail.com>
2025-07-18 13:16:59 +0100

    update config

--- src/data/scavenger-hunt-config.ts
+++ src/data/scavenger-hunt-config.ts
@@ -1,7 +1,4 @@
 import { ScavengerHuntConfig } from '@/types/scavenger-hunt';
-import treasureChest from '@/assets/treasure-chest.png';
-import searchTask from '@/assets/search-task.png';
-import starTask from '@/assets/star-task.png';
 import pikachuSummerHat from '@/assets/pikachu-summer-hat.png';
 import alolanGeodude from '@/assets/alolan-geodude.png';
 import wooloo from '@/assets/wooloo.png';
@@ -9,15 +6,36 @@ import sandshrew from '@/assets/sandshrew.png';
 import phanpy from '@/assets/phanpy.png';
 import venipede from '@/assets/venipede.png';
 import golett from '@/assets/golett.png';
-import varoom from '@/assets/varoom.png';
+import alolanGolem from '@/assets/alolan-golem.png';
 import voltorb from '@/assets/voltorb.png';
 import foongus from '@/assets/foongus.png';
 import galarianStunfisk from '@/assets/galarian-stunfisk.png';
 import unown from '@/assets/unown.png';
 import electrode from '@/assets/electrode.png';
 import terrakion from '@/assets/terrakion.png';
-import xxlLetters from '@/assets/xxl-letters.png';
-import shinySparkles from '@/assets/shiny-sparkles.png';
+import xxlLetters from '@/assets/xxl-letters.jpg';
+import shinySparkles from '@/assets/shiny-sparkles.jpg';
+import buddyhearts from '@assets/buddy-hearts.jpg';
+import shard from 'assets/shard.jpg';
+import tower from 'asets/tower.jpg';
+import towerBridge from 'asets/tower-bridge.jpg';
+import monument from 'asets/monument.jpg';
+import gym from 'asets/gym.png';
+import pokestop from 'asets/pokestop.jpg';
+import evolveRaichu from 'asets/evolve-raichu.png';
+import camera from 'asets/camera.jpg';
+import dynamax from 'asets/dynamax.png';
+import checkin from 'asets/check-in.jpg';
+import gift from 'asets/gift.png';
+import tradeDistance from 'asets/trade-distance.png';
+import party from 'asets/party.jpg';
+import friend from 'asets/friend.jpg';
+import buddyhearts from 'asets/buddy-hearts.jpg';
+import raid from 'asets/raid.jpg';
+import rocket from 'asets/rocket.jpg';
+
+
+
 
 export const scavengerHuntConfig: ScavengerHuntConfig = {
   title: "Epic Scavenger Hunt",
@@ -80,7 +98,7 @@ export const scavengerHuntConfig: ScavengerHuntConfig = {
         {
           id: "varoom",
           title: "Evolve An Alolan Geodude Into Alolan Graveler",
-          image: varoom,
+          image: alolanGolem,
           points: 5,
           completed: false
         },
@@ -149,42 +167,42 @@ export const scavengerHuntConfig: ScavengerHuntConfig = {
         {
           id: "road-trip-pokestop",
           title: "Spin The Photodisc On A Road Trip Pokestop",
-          image: searchTask,
+          image: pokestop,
           points: 5,
           completed: false
         },
         {
           id: "road-trip-gym",
           title: "Spin The Photodisc On A Road Trip Gym",
-          image: searchTask,
+          image: gym,
           points: 5,
           completed: false
         },
         {
           id: "shard-gym",
           title: "Spin The Photodisc On \"The Shard\" Gym",
-          image: searchTask,
+          image: shard,
           points: 15,
           completed: false
         },
         {
           id: "tower-bridge-gym",
           title: "Spin The Photodisc On The \"Tower Bridge\" Gym",
-          image: searchTask,
+          image: towerBridge,
           points: 15,
           completed: false
         },
         {
           id: "london-monument-gym",
           title: "Spin The Photodisc On \"The London Monument\" Gym",
-          image: searchTask,
+          image: monument,
           points: 15,
           completed: false
         },
         {
           id: "tower-of-london-gym",
           title: "Spin The Photodisc On One Of The \"Tower Of London\" Gyms",
-          image: searchTask,
+          image: tower,
           points: 15,
           completed: false
         }
@@ -197,56 +215,56 @@ export const scavengerHuntConfig: ScavengerHuntConfig = {
         {
           id: "defeat-team-rocket",
           title: "Defeat A Team Rocket Grunt",
-          image: starTask,
+          image: rocket,
           points: 5,
           completed: false
         },
         {
           id: "win-raid-battle",
           title: "Win A Raid Battle",
-          image: starTask,
+          image: raid,
           points: 5,
           completed: false
         },
         {
           id: "win-max-battle",
           title: "Win A Max Battle",
-          image: starTask,
+          image: dynamax,
           points: 5,
           completed: false
         },
         {
           id: "snapshot-with-buddy",
           title: "Take A Snapshot With Summer Hat Pikachu As Your Buddy",
-          image: starTask,
+          image: camera
           points: 5,
           completed: false
         },
         {
           id: "hatch-an-egg",
           title: "Hatch An Egg",
-          image: starTask,
+          image: egg,
           points: 5,
           completed: false
         },
         {
           id: "evolve-summer-pikachu",
           title: "Evolve A Summer Hat Pikachu",
-          image: starTask,
+          image: evolveRaichu,
           points: 5,
           completed: false
         },
         {
           id: "evolve-alolan-graveler",
           title: "Evolve An Alolan Graveler Into An Alolan Golem",
-          image: starTask,
+          image: alolanGolem,
           points: 15,
           completed: false
         },
         {
           id: "earn-buddy-hearts",
           title: "Earn 7 Buddy Hearts With Summer Hat Pikachu As Your Buddy",
-          image: starTask,
+          image: buddyhearts,
           points: 15,
           completed: false
         }
@@ -259,42 +277,42 @@ export const scavengerHuntConfig: ScavengerHuntConfig = {
         {
           id: "make-new-friend",
           title: "Make A New Friend",
-          image: starTask,
+          image: friend,
           points: 25,
           completed: false
         },
         {
           id: "join-party",
           title: "Join A Party",
-          image: starTask,
+          image: party,
           points: 25,
           completed: false
         },
         {
           id: "ar-photo-tower-bridge",
           title: "Take An AR Photo With Tower Bridge In The Background",
-          image: starTask,
+          image: camera,
           points: 25,
           completed: false
         },
         {
           id: "trade-5000km",
           title: "Make A Trade With A Distance Of 5000km Or More",
-          image: starTask,
+          image: tradeDistance,
           points: 25,
           completed: false
         },
         {
           id: "send-road-trip-gift",
           title: "Send A Gift With A Road Trip Postcard",
-          image: starTask,
+          image: gift,
           points: 25,
           completed: false
         },
         {
           id: "campfire-checkin",
           title: "Check In To The Scavenger Hunt On Campfire",
-          image: starTask,
+          image: checkin,
           points: 25,
           completed: false
         }
